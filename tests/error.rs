14dc44405ab0c85749b77c5c9a57f4982b467a1f
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-11-03 01:54:46 +0000

    Refactor tests to use sqlx::test

--- tests/error.rs
+++ tests/error.rs
@@ -5,30 +5,6 @@ pub enum SpawnAppErr {
     #[error("Error listening on TCP port: {source}")]
     Listen { source: std::io::Error },
 
-    #[error("Error getting configuration: {source}")]
-    GetConfiguration { source: config::ConfigError },
-
     #[error("Error getting the listen port: {source}")]
     GetListenPort { source: std::io::Error },
-
-    #[error("Error configuring the database: {source}")]
-    ConfigureDatabase { source: ConfigureDatabaseError },
-}
-
-#[derive(Error, Debug)]
-pub enum ConfigureDatabaseError {
-    #[error("Error connecting to Postgres: {source}")]
-    PostgresConnection { source: sqlx::Error },
-
-    #[error("Error creating the database '{db_name}': {source}")]
-    CreateDatabase {
-        source: sqlx::Error,
-        db_name: String,
-    },
-
-    #[error("Postgres Pool connection error: {source}")]
-    PostgresPool { source: sqlx::Error },
-
-    #[error("Error while running database migrations: {source}")]
-    Migrations { source: sqlx::migrate::MigrateError },
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,67 +1,32 @@
 mod error;
 
 use color_eyre::Result;
-use error::{ConfigureDatabaseError, SpawnAppErr};
-use sqlx::{Connection, Executor, PgConnection, PgPool};
+use error::SpawnAppErr;
+use sqlx::PgPool;
 use std::net::TcpListener;
-use uuid::Uuid;
-use zero2prod::configuration::{get_configuration, DatabaseSettings};
 
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
 }
 
-pub async fn configure_database(
-    config: &DatabaseSettings,
-) -> Result<PgPool, ConfigureDatabaseError> {
-    let maintenance_settings = DatabaseSettings {
-        database_name: "postgres".to_string(),
-        password: "password".to_string(),
-        username: "postgres".to_string(),
-        ..config.clone()
-    };
-
-    let mut connection = PgConnection::connect(&maintenance_settings.connection_string())
-        .await
-        .map_err(|source| ConfigureDatabaseError::PostgresConnection { source })?;
-
-    connection
-        .execute(format!("CREATE DATABASE \"{}\"", config.database_name).as_str())
-        .await
-        .map_err(|source| ConfigureDatabaseError::CreateDatabase {
-            source,
-            db_name: config.database_name.clone(),
-        })?;
-
-    let pg_pool = PgPool::connect(&config.connection_string())
-        .await
-        .map_err(|source| ConfigureDatabaseError::PostgresPool { source })?;
-
-    sqlx::migrate!("./migrations")
-        .run(&pg_pool)
-        .await
-        .map_err(|source| ConfigureDatabaseError::Migrations { source })?;
-
-    Ok(pg_pool)
-}
-
-async fn spawn_app() -> Result<TestApp, SpawnAppErr> {
+async fn spawn_app(db_pool: PgPool) -> Result<TestApp, SpawnAppErr> {
     let listener =
         TcpListener::bind("127.0.0.1:0").map_err(|source| SpawnAppErr::Listen { source })?;
     let port = listener
         .local_addr()
         .map_err(|source| SpawnAppErr::GetListenPort { source })?
         .port();
-    let mut configuration =
-        get_configuration().map_err(|source| SpawnAppErr::GetConfiguration { source })?;
+    // let mut configuration =
+    //     get_configuration().map_err(|source| SpawnAppErr::GetConfiguration { source })?;
 
-    configuration.database.database_name = Uuid::new_v4().to_string();
+    // configuration.database.database_name = Uuid::new_v4().to_string();
 
-    let db_pool = configure_database(&configuration.database)
-        .await
-        .map_err(|source| SpawnAppErr::ConfigureDatabase { source })?;
+    // let db_pool = configure_database(&configuration.database)
+    //     .await
+    //     .map_err(|source| SpawnAppErr::ConfigureDatabase { source })?;
 
+    // *TODO* Fix expect
     let server =
         zero2prod::startup::run(listener, db_pool.clone()).expect("Failed to bind address");
     let server_task = tokio::spawn(server);
@@ -72,10 +37,9 @@ async fn spawn_app() -> Result<TestApp, SpawnAppErr> {
     Ok(TestApp { address, db_pool })
 }
 
-#[tokio::test]
-async fn health_check_works() -> Result<()> {
-    // color_eyre::install()?;
-    let test_app = spawn_app().await?;
+#[sqlx::test]
+async fn health_check_works(pool: PgPool) -> Result<()> {
+    let test_app = spawn_app(pool.clone()).await?;
     let address = test_app.address;
 
     let client = reqwest::Client::new();
@@ -91,9 +55,9 @@ async fn health_check_works() -> Result<()> {
     Ok(())
 }
 
-#[tokio::test]
-async fn subscribe_returns_a_200_for_valid_form_data() -> Result<()> {
-    let test_app = spawn_app().await?;
+#[sqlx::test]
+async fn subscribe_returns_a_200_for_valid_form_data(pool: PgPool) -> Result<()> {
+    let test_app = spawn_app(pool.clone()).await?;
     let address = test_app.address;
 
     let client = reqwest::Client::new();
@@ -108,7 +72,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() -> Result<()> {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&test_app.db_pool)
+        .fetch_one(&pool)
         .await?;
 
     assert_eq!(saved.email, "not_my_email@nomail.com");
@@ -116,9 +80,9 @@ async fn subscribe_returns_a_200_for_valid_form_data() -> Result<()> {
     Ok(())
 }
 
-#[tokio::test]
-async fn subscribe_returns_400_when_data_is_missing() -> Result<()> {
-    let test_app = spawn_app().await?;
+#[sqlx::test]
+async fn subscribe_returns_400_when_data_is_missing(pool: PgPool) -> Result<()> {
+    let test_app = spawn_app(pool.clone()).await?;
     let address = test_app.address;
     let client = reqwest::Client::new();
     let test_cases = vec![
