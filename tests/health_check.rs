00b33d3c9276fb3e25cc61c625738bbaf4455b96
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-07-14 08:07:22 +0100

    Fix format string

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -102,8 +102,7 @@ async fn subscribe_returns_400_when_data_is_missing(pool: PgPool) -> Result<()> 
         assert_eq!(
             400,
             response.status().as_u16(),
-            "The API did not fail with 400 Bad Request when the payload was {}.",
-            error_message
+            "The API did not fail with 400 Bad Request when the payload was {error_message}.",
         );
     }
     Ok(())
